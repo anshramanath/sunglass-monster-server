49b109d53ed669063cef3c159f80a0ff0996d398
Ansh Ramanath <165216397+anshramanath@users.noreply.github.com>
2026-06-13 01:49:17 -0500

    Delete prosport-frontend directory

--- prosport-frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "zgcekcoatiskqbdruadg.supabase.co" }],
-  },
-};
-
-export default nextConfig;
